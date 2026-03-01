c9250e600ad706babf50be36fe3d1e6df079ff38
Golor23 <carlbenedick123@gmail.com>
2026-03-01 22:33:02 +0800

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const jobs = [
     },
     {
         title: 'Cook',
-        img: 'Driver.png',
+        img: '/main/Driver.png',
         description: 'Prepare meals according to instructions.',
         contact: 'Ms. Reyes - 0917-555-5678',
     },
@@ -94,6 +94,7 @@ jobModal.addEventListener('click', e => {
         jobModal.classList.add('hidden');
     }
 });
+
 
 
 
