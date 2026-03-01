f2259682cf8df7481fd8862bc39e5ff92635ca1b
Golor23 <carlbenedick123@gmail.com>
2026-03-01 22:05:15 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const jobs = [
     {
         title: 'Driver',
-        img: 'Driver.png',
+        img: '/Driver.png',
         description: 'Responsible for transporting goods safely.',
         contact: 'Mr. Santos - 0917-555-1234',
     },
@@ -94,5 +94,6 @@ jobModal.addEventListener('click', e => {
         jobModal.classList.add('hidden');
     }
 });
+
 
 
