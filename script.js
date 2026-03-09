3a4fd6816d561edc596d199a540050bf7444d066
Golor23 <carlbenedick123@gmail.com>
2026-03-09 18:08:53 +0800

    Update job image path in jobs array

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const jobs = [
     {
         title: 'Driver',
-        img: '/again/Driver.png',
+        img: 'README.md',
         description: 'Responsible for transporting goods safely.',
         contact: 'Mr. Santos - 0917-555-1234',
     },
@@ -93,6 +93,7 @@ jobModal.addEventListener('click', e => {
         jobModal.classList.add('hidden');
     }
 });
+
 
 
 
